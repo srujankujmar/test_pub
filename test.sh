b981137fb88c54fd32b5ca2cd6dc49bbdbf180b1
Srujankumar5140 <62127952+Srujankumar5140@users.noreply.github.com>
2020-03-30 18:04:48 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Helloaaa"
+echo "Testaing the execution of POLLING SCM"
+a=`echo ${GIT_BRANCH} | sed -e "s|origin/||g"`
+#git_branch_local = $(echo $GIT_BRANCH   | sed -e "s|origin/||g")
+echo $a
+echo "build user is"
+echo ${BUILD_USER}
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                                                                                                                                                                                                           
+~                              
