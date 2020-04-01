93baf43d28c2d3f9fa028d965d09a2b8da21c896
srujankumar5140 <srujankumar.gourishetty@gmail.com>
2020-04-01 09:17:48 +0530

    testushushushush

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello Mr.Srujankumar Gourishetty Hi" 
+echo "Hello Mr.Srujankumar Gourishetty " 
 echo "Testaing the execution of POLLING SCM"
 a=`echo ${GIT_BRANCH} | sed -e "s|origin/||g"`
 #git_branch_local = $(echo $GIT_BRANCH   | sed -e "s|origin/||g")
