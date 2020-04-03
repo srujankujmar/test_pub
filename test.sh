d3d80c4b3c5167d7e2eb162da234ce1d7bb36258
srujankujmar <61962773+srujankujmar@users.noreply.github.com>
2020-04-03 15:29:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 echo "Hello Mr.Srujankumar testing updated PR"
 echo "Testaing the execution of POLLING SCM"
 a=`echo ${GIT_BRANCH} | sed -e "s|origin/||g"`
+
+aaaa
 #git_branch_local = $(echo $GIT_BRANCH   | sed -e "s|origin/||g")
 echo $a
 echo "build user is"
