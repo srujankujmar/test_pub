1acd28b1b9a969d07f4a120407cef4135e3d807f
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-06 09:07:22 +0530

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello Mr.Srujankumar testing updated PR"
+echo "Hello aMr.Srujankumar testing updated PR"
 echo "Testaing the execution of POLLING SCM"
 a=`echo ${GIT_BRANCH} | sed -e "s|origin/||g"`
 
