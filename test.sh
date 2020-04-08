4b183e0f64473b2f74b91dd13dffae04dc8e0e8e
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-08 08:21:51 +0530

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "Hello MR.Srujankumar testing updated PR"
+echo "Hello  MR.Srujankumar testing updated PR"
 echo "Testaing the execution of POLLING SCM"
 a=`echo ${GIT_BRANCH} | sed -e "s|origin/||g"`
-
-aaaa
+sleep 6000
 #git_branch_local = $(echo $GIT_BRANCH   | sed -e "s|origin/||g")
 echo $a
 echo "build user is"
