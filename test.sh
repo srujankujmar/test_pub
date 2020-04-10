be487ee9f0296012d042220c67d7fe6da099d63a
srujankujmar <srujan.gourishetty@hyscale.io>
2020-04-10 16:05:32 +0530

    Removing workflow yamls from build ignore and Attaching git log to mail

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Hello  MR.Srujankumar testing updated PR"
-echo "Testaing the execution of POLLING SCM"
+echo "Tesaataing the execution of POLLING SCM"
 a=`echo ${GIT_BRANCH} | sed -e "s|origin/||g"`
 #git_branch_local = $(echo $GIT_BRANCH   | sed -e "s|origin/||g")
 echo $a
